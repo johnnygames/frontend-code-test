67725560e6f9c6d77a7a1517569418e10e0e7fb7
John Games <johnegames@gmail.com>
2015-08-11 19:26:20 -0700

    (feat) Basic 'hello world' implementation

---
+++ webpack.config.js
@@ -0,0 +1,20 @@
+var path = require('path');
+
+module.exports = {
+  resolve: {
+    modulesDirectories: ['node_modules'],
+    alias: {},
+    extensions: ['', '.jsx', '.js']
+  },
+  entry: ['webpack/hot/dev-server', path.resolve(__dirname, 'app/main.js'k],
+  output: {
+    path: path.resolve(__dirname, 'build'),
+    filename: "bundle.js"
+  },
+  module: {
+    loaders: [{
+      test: /\.jsx?$/, // A regexp to test the require path. accepts either js or jsx
+      loader: 'babel' // The module to load. "babel" is short for "babel-loader"
+    }]
+  }
+};
